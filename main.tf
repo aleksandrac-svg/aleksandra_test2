ba40604b251aef8d0ce12c886e4aac8512711e01
aleksandrac-svg <aleksandrac@spacelift.io>
2026-03-06 12:53:59 +0100

    Refactor Terraform to add VPC and main subnet resources
    Removed unused subnet variable and data sources. Added VPC and subnet resources with appropriate configurations.

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-variable "subnet_id" {
-  type        = string
-  description = "ID of the subnet from networking stack"
-}
-
 terraform {
   required_providers {
     aws = {
@@ -16,21 +11,25 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_subnet" "selected" {
-  id = var.subnet_id
-}
-
-data "aws_vpc" "selected" {
-  id = data.aws_subnet.selected.vpc_id
-}
-
-resource "aws_security_group" "app" {
-  name        = "orbit-labs-app-sg"
-  description = "Security group for Orbit Labs app"
-  vpc_id      = data.aws_vpc.selected.id
-
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
   tags = {
-    name    = "Orbit Labs App SG"
+    name    = "Orbit Labs VPC"
     project = "Orbit-labs"
   }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    name    = "Orbit Labs Subnet"
+    project = "Orbit-labs"
+  }
+}
+
+output "subnet_id" {
+  value       = aws_subnet.main.id
+  description = "ID of the main subnet"
 }
